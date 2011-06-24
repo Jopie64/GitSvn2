b08935d9a72c9e8928837fd9f1f934124f504c04
Johan 't Hart <johanthart@gmail.com>
2011-06-24 18:29:26 +0200

    Included libgit2 and GitCpp in build process

--- GitSvn.cpp
+++ GitSvn.cpp
@@ -3,9 +3,11 @@
 
 #include "stdafx.h"
 
+#include <windows.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	HMODULE libsvn = LoadLibrary(L"C:\\Program Files\\Subversion\\libsvn_wc-1.dll");
 	return 0;
 }
 
